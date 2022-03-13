2df3ad1b3b530632031ed8f14b10d0db08c5775a
Bhavana <bhavananerlakota@gmail.com>
2022-03-13 15:18:01 +0530

    added UC2 file

--- Employee1.cs
+++ Employee1.cs
@@ -10,6 +10,7 @@ namespace EmployeeWage
     {
         public void UC1Employee()
         {
+            Console.WriteLine("Welcome to UC1");
             Random random = new Random();
             int num = random.Next(0, 2);
             if (num == 0)
@@ -21,6 +22,28 @@ namespace EmployeeWage
                 Console.WriteLine("Employee is absent");
 
 
+            }
+
+
+        }
+        public void UC2DailyEmployeeWage()
+        {
+            Console.WriteLine("Welcome to UC2");
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0; 
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is:" + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Total daily wage is:" + dailyWage);
             }
 
         }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace EmployeeWage
         {
             Employee1 emp = new Employee1();
             emp.UC1Employee();
-           
+            emp.UC2DailyEmployeeWage();
+
 
 
 
